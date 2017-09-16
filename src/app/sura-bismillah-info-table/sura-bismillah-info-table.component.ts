c56ea343e794eae5667dba335417d5ad48737554
Amin Azarbadegan <amin@azarbadegan.net>
2017-09-16 17:38:49 +0430

    1. Bug in moving back and forth when reaching start/end of quran - fixed 2. Bug: Removing link to script in index.html (not there)

--- src/app/sura-bismillah-info-table/sura-bismillah-info-table.component.ts
+++ src/app/sura-bismillah-info-table/sura-bismillah-info-table.component.ts
@@ -9,12 +9,13 @@ import { QuranService } from "../quran.service";
 export class SuraBismillahInfoTableComponent implements OnInit {
   @Input() bismillahText = '';
   @Input() suraname = '';
+  @Input() suraNumber;
   suraAyaNumber : number=0;
   suraTanzilLocation;
   suraArabicName;
   imgflag;
   nightMode=false;
-   
+
   constructor(private quranService: QuranService) {
   }
 
